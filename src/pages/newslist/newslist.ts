049de5878ff7f03879c719a4c4f47eba5f83b191
nmgwr <nmgwr1216@163.com>
2018-02-24 03:28:37 +0800

    完善了下拉刷新和上啦加载

--- src/pages/newslist/newslist.ts
+++ src/pages/newslist/newslist.ts
@@ -12,6 +12,7 @@ export class NewslistPage {
   pageIndex=1;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.pageIndex = 1;
     // 初始化加载三条未读假数据
     let arr = [];
     for(let i = 0; i < 3; i++){
@@ -34,7 +35,7 @@ export class NewslistPage {
   // 获取新闻列表
   getNewsList(){
     let arr = this.newsList;
-    for(let i = 0; i < 15; i++){
+    for(let i = 0; i < 5; i++){
       arr.push({
         title:"企业OA新闻业开发正在进行中，看看标题能打多少字",
         content:"这块运用了ionic的ion-list组件,增加了些自己的样式",
@@ -46,11 +47,16 @@ export class NewslistPage {
     this.newsList = arr;
   }
 
-  // 下一页
+  // 下一页 上拉加载
   nextPage(infiniteScroll){
-    console.log("加载第"+this.pageIndex+"页");
+    console.log("加载第"+this.pageIndex+"页" + "目前共有数据" + this.newsList.length + "条");
+    if(this.newsList.length > 20){
+      console.log("没有更多数据！！");
+      infiniteScroll.enable(false);
+    }
     this.pageIndex++;
     setTimeout(() => {
+      this.getNewsList();
       console.log('上拉加载结束');
       infiniteScroll.complete();
     }, 2000);
@@ -59,7 +65,7 @@ export class NewslistPage {
   // 下拉刷新
   doRefresh(refresher){
     console.log("下拉刷新开始");
-    this.pageIndex = 1;
+    this.constructor();
     setTimeout(() => {
       console.log('下拉属性结束');
       refresher.complete();
